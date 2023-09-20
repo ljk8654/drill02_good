f2385424d3712b5bcbb76db0fd759bbb88b8aa4d
ljk8654 <132908803+ljk8654@users.noreply.github.com>
2023-09-20 18:13:19 +0900

    화면 그리는 것 함수화

--- character_move.py
+++ character_move.py
@@ -6,28 +6,31 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def rander_all(x,y):
+    clear_canvas_now()
+    grass.draw_now(400,30)
+    character.draw_now(x,y)
+    delay(0.01)
+   
+
 def run_circle():
     print("circle")
     cx, cy, r = 800 / 2, 300 , 200
     for deg in range(0, 360, 5):
         x = cx + r * math.cos( math.radians(deg))
         y = cy + r* math.sin(math.radians(deg))
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,y)
-        delay(0.1)
-
+        rander_all(x,y)
+       
 def run_rectangle():
     print("rectangle")
     #bottom line
     for x in range(0,750+1,10):
-        clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,90)
-        delay(0.1)
-
+        rander_all(x,90)
+    for x in range(750, 50, -10):
+        rander_all(x,90)
+    
 while True:
-    # run_circle()
+    run_circle()
     run_rectangle()
     break
 
